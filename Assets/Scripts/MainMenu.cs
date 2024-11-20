77fb3ad61cd2f437b84c01a95a7d9158e3c5a2ba
JedTyde <59925703+JedTyde@users.noreply.github.com>
2024-11-20 13:11:17 +0100

    maten a john cuadrado

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,13 +4,55 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MainMenu : MonoBehaviour
 {
     public Button button1;
     public Button button2;
     public Button button3;
+    public Image day;
+    public Image night;
+    public TextMeshProUGUI titleDay;
+    public TextMeshProUGUI titlenight;
+    public TextMeshProUGUI subTitleDay;
+    public TextMeshProUGUI subTitleNight;
+    //private DateTime time;
 
+    void Update()
+    {
+        // Get the current time
+        DateTime currentTime = DateTime.Now;
+        TimeSpan currentTimeOfDay = currentTime.TimeOfDay;
+
+        // Define the start and end times for night
+        TimeSpan nightStart = new TimeSpan(18, 0, 0); // 6 PM
+        TimeSpan nightEnd = new TimeSpan(6, 0, 0); // 6 AM
+
+        // Check if the current time is within the night period
+        bool isNight = currentTimeOfDay >= nightStart || currentTimeOfDay < nightEnd;
+
+        // Output the result
+        if (isNight == false)
+        {
+            day.gameObject.SetActive(true);
+            titleDay.gameObject.SetActive(true);
+            subTitleDay.gameObject.SetActive(true);
+            night.gameObject.SetActive(false);
+            titlenight.gameObject.SetActive(false);
+            subTitleNight.gameObject.SetActive(false);
+        }    
+        if (isNight == true)
+        {
+            day.gameObject.SetActive(false);
+            titleDay.gameObject.SetActive(false);
+            subTitleDay.gameObject .SetActive(false);
+            night.gameObject.SetActive(true);
+            titlenight.gameObject.SetActive(true);
+            subTitleNight.gameObject.SetActive(true);
+        }
+        //Debug.Log("Is it night? " + isNight);
+    }
     public void level1()
     {
         SceneManager.LoadScene(1);
